881531a1ba40ed4c900bc1d88594dc66030bc3d4
Edwin J.E. Delph <delphedwin@gmail.com>
2020-07-13 11:24:35 -0700

    resolve ERROR in HostResourceLoader

--- src/app/hex-gl/hex-gl.component.ts
+++ src/app/hex-gl/hex-gl.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hex-gl',
   templateUrl: './hex-gl.component.html',
-  styleUrls: ['./hex-gl.component.css']
+  styleUrls: ['./hex-gl.component.css', './css/multi.css', './css/fonts.css']
 })
 export class HexGLComponent implements OnInit {
 
